a3d6760cdbe66bdc032225a6192eb6c237037c5a
ivankahnybediuk <56602081+ivankahnybediuk@users.noreply.github.com>
2020-04-05 14:28:57 +0300

    1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // 1. Создать страницу, которая выводит нумерованный список песен:
 let playList = [
     {author: "LED ZEPPELIN", song:"STAIRWAY TO HEAVEN"},
@@ -10,4 +11,45 @@ let playList = [
     {author: "METALLICA",song:"ENTER SANDMAN"}
 ];
 let list = document.getElementById("playList");
-list.innerte
+let html=`<ol>`;
+for(let i=0; i<=playList.length-1; i++){
+    html+=`<li> Author: ${playList[i]["author"]}, "${playList[i]["song"]}"`
+}
+html+=`</ol>`;
+list.innerHTML=html;
+
+
+// 2. Создать HTML-страницу с кнопкой "Открыть" и модальным окном. На модальном окне должен быть текст и кнопка "Закрыть". Изначально модальное окно не отображается. При клике на кнопку "Открыть" появляется модальное окно, на кнопку "Закрыть" – исчезает.
+let showModalWindow = function(state){
+    document.getElementById('modal_window').style.display='block';
+    document.getElementById('filter').style.display='block';
+};
+let closeModalWindow = function(state){
+    document.getElementById('modal_window').style.display="none";
+    document.getElementById('filter').style.display="none";
+};
+
+// 3. Создать HTML-страницу со светофором и кнопкой, которая переключает светофор на следующий цвет.
+let clearColor=function(){
+    document.getElementById('red_').style.backgroundColor = "black";
+    document.getElementById('yellow_').style.backgroundColor = "black";
+    document.getElementById('green_').style.backgroundColor = "black";
+};
+let changeColor =function(){
+    for(let click = 1; click<=3; click++){
+    switch(click){
+    case 1:
+        clearColor();
+        document.getElementById('red_').style.backgroundColor = "red";
+        break;
+    case 2:
+        clearColor();
+        document.getElementById('yellow_').style.backgroundColor = "yellow";
+        break
+    case 3:
+        clearColor();
+        document.getElementById('green_').style.backgroundColor = "green";
+        break
+    }
+}
+}
